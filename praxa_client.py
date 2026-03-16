1abf845710188128480e389c6a3447f7ecd8ac1d
Wendy Wanjiru <wendywanjiru18@gmail.com>
2026-03-16 15:16:54 +0300

    Updated the rag pipeline and created stremlit ui

---
+++ praxa_client.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import praxa_rag
+import random
+import time
+
+
+st.title("Praxa")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    question = prompt
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(question)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response =praxa_rag.answer_and_sources(question)
+        st.markdown(response["answer"])
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
